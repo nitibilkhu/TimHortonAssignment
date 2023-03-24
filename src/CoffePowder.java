102c781f0a886271164f323d8110a0de548d0f30
nitibilkhu <113490933+nitibilkhu@users.noreply.github.com>
2023-03-24 01:01:38 -0400

    Content added

--- src/CoffePowder.java
+++ src/CoffePowder.java
@@ -1,7 +1,7 @@
 public class CoffePowder implements TimMerchandise{
     @Override
     public String toString() {
-        return "Pure coffee powder is light in weight, has a relatively uniform porosity, smoothness, and is not sticky or lumpy.";
+        return "Pure coffee powder is light in weight, has a relatively uniform porosity, smoothness, and is not sticky or lumpy Pure coffee powder is light in weight, has a relatively uniform porosity, smoothness, and is not sticky or lumpy..";
     }
     @Override
     public String getFeature() {
